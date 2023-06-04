117f1ec094c6ebac783eed282c9170f5d6e793d6
M_Nahuel <91757112+M-Nahuel@users.noreply.github.com>
2023-06-03 20:46:07 -0400

    post put patch delete methods added

--- back/routers/usersRouter.js
+++ back/routers/usersRouter.js
@@ -18,3 +18,51 @@ routerUsers.get('/employees', (req, res) => {
     res.json(users.employees);
 });
 
+///////Add logic to verify if is a customer or an employee
+routerUsers.post('/', (req, res) => {
+    let newUser = req.body;
+    users.push(newUser);
+    res.json(users);
+});
+
+///////
+routerUsers.put('/:id', (req,res) => {
+    const modUser = req.body;
+    const id = req.params.id;
+
+    const index = users.findIndex(users => users.id == id);
+
+    if (index >= 0) {
+        users[index] = modUser;
+    }
+
+    res.json(users);
+});
+
+/////
+routerUsers.patch('/:id', (req, res) => {
+    const modUser = req.body;
+    const id = req.params.id;
+
+    const index = users.findIndex(users => users.id == id);
+
+    if (index >= 0) {
+        Object.assign(users[index], modUser);
+    }
+
+    res.json(users);
+});
+
+////
+routerUsers.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const index = users.findIndex(users => users.id == id);
+
+    if (index >= 0) {
+        users.splice(index, 1);
+    }
+
+    res.json(users);
+});
+
+module.exports = routerUsers;
